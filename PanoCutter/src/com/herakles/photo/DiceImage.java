b40e651e4783daa11b77482f71622fa7938177e0
yrajaram <yrajaram@.cisco.com>
2016-07-27 12:40:52 -0700

    Making it easy to use

--- PanoCutter/src/com/herakles/photo/DiceImage.java
+++ PanoCutter/src/com/herakles/photo/DiceImage.java
@@ -1,6 +1,5 @@
 package com.herakles.photo;
 
-import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.geom.AffineTransform;
 import java.awt.image.AffineTransformOp;
@@ -12,9 +11,17 @@ import java.io.IOException;
 import javax.imageio.ImageIO;
 
 public class DiceImage {
-	static final int RequiredScaleFactor = 1;
-
+	private static enum TileSize {	// Available sizes for the tiles
+		p4x6, p5x7, p8x12, p12x18  
+	}
+	
+	//---------------Change only the three below to get desired results
+	static final TileSize PhotoSize = TileSize.p4x6;   // What is each tile size?
+	static final int RequiredScaleFactor = 1;          // By how much the image should be scaled?
+	static final String ImageFileName = "SFO.jpg";     // What is the image file name?
+	
     public static void main(String[] args) throws IOException {
+    	
     	/*
     	 * from: http://www.montclairphoto.com/resolution.html
     	 * 
@@ -28,9 +35,23 @@ public class DiceImage {
 			8x12	-	2434x3638
 			10x15	-	3036x4536
     	 */
-    	int Height5x7 = 2138, Length5x7 = 1524; // Make a collage of 5x7 images
-
-        File file = new File("SFO.jpg");
+    	int tileHeight = 2138, tileLength = 1524; // Make a collage of 5x7 images
+    	
+    	switch (PhotoSize){
+    	case p4x6:
+    		tileHeight = 1818; tileLength = 1228;
+    		break;
+    	case p5x7:
+    		tileHeight = 2138; tileLength = 1524;
+    		break;
+    	case p8x12:
+    		tileHeight = 3638; tileLength = 2434;
+    		break;
+    	case p12x18:
+    		tileHeight = 5454; tileLength = 3684;
+    		break;
+    	}
+        File file = new File(ImageFileName);
         FileInputStream fis = new FileInputStream(file);
         BufferedImage image = ImageIO.read(fis);
         
@@ -38,36 +59,36 @@ public class DiceImage {
         int picLength = image.getWidth();
         float scaleFactor = 0.0f;
         if (picHeight<picLength) {
-        	Height5x7 ^= Length5x7; Length5x7 ^= Height5x7; Height5x7 ^= Length5x7; // matching dimensions
-        	scaleFactor = picHeight/((float)Height5x7);
+        	tileHeight ^= tileLength; tileLength ^= tileHeight; tileHeight ^= tileLength; // matching dimensions
+        	scaleFactor = picHeight/((float)tileHeight);
         } else {
-        	scaleFactor = picLength/((float)Length5x7);
+        	scaleFactor = picLength/((float)tileLength);
         }
         
         if (scaleFactor>=1){
         	image = scaleImage(image, RequiredScaleFactor*((float)((int)scaleFactor)/scaleFactor));
-        }    else {
+        } else {
         	image = scaleImage(image, RequiredScaleFactor/scaleFactor);
         }
 
-        int rows = image.getHeight()/Height5x7; 
-        int cols = image.getWidth()/Length5x7;
+        int rows = image.getHeight()/tileHeight; 
+        int cols = image.getWidth()/tileLength;
         
-        rows = ((image.getHeight() % Height5x7)!=0)?++rows:rows;
-        cols = ((image.getWidth() % Length5x7)!=0)?++cols:cols;
+        rows = ((image.getHeight() % tileHeight)!=0)?++rows:rows;
+        cols = ((image.getWidth() % tileLength)!=0)?++cols:cols;
                 
         System.out.println("Rows:"+rows+" Cols:"+cols);
            
-        BufferedImage img = new BufferedImage(Length5x7, Height5x7, image.getType());
+        BufferedImage img = new BufferedImage(tileLength, tileHeight, image.getType());
         Graphics2D gr = null;
         gr = img.createGraphics();
     	
         for (int x = 0; x < rows; x++) {
             for (int y = 0; y < cols; y++) {
             	if (y == cols-1) {
-            		gr.clearRect(0, 0, Length5x7, Length5x7);
+            		gr.clearRect(0, 0, tileLength, tileLength);
             	}
-                gr.drawImage(image, 0, 0, Length5x7, Height5x7, Length5x7 * y, Height5x7 * x, Length5x7 * y + Length5x7, Height5x7 * x + Height5x7, null);
+                gr.drawImage(image, 0, 0, tileLength, tileHeight, tileLength * y, tileHeight * x, tileLength * y + tileLength, tileHeight * x + tileHeight, null);
                 ImageIO.write(img, "jpg", new File(file.getName().substring(0, file.getName().indexOf("."))+"-tile-" + x + y + ".jpg"));
             }
         }
